b8f42b064703c17ae1ae6e41bf05b2f4ec543841
PissMidas <136759945+PissMidas@users.noreply.github.com>
2024-10-21 14:56:23 -0400

    Update tk.py

--- tk.py
+++ tk.py
@@ -7,6 +7,8 @@ import re
 import sys
 import pygetwindow
 import time
+import requests
+from bs4 import BeautifulSoup
 from PIL import Image, ImageTk
 
 #pyinstaller --onefile --icon=images/dubu.ico --add-data "images;images" --noconsole --name "AwakeningTracker" tk.py
@@ -15,6 +17,45 @@ from PIL import Image, ImageTk
 #pyinstaller --onedir --icon=images/dubu.ico --add-data "images;images" --noconsole --name "AwakeningTracker" tk.py
 #alternative command line to compile. (antivirus less likely to have a false positive)
 
+def extract_and_replace_awakenings(url):
+    # Send a request to get the HTML of the page
+    # Dictionary that maps internal identifiers to external names
+    dict_external_to_internal = {"Glass Cannon": "TD_AvoidDamageHitHarder", "Demolitionist": "TD_BarrierBuff", "Reptile Remedy": "TD_BaseStaggerAndRegen", "Spark of Focus": "TD_BlessingCooldownRate", "Spark of Resilience": "TD_BlessingMaxStagger", "Spark of Strength": "TD_BlessingPower", "Spark of Leadership": "TD_BlessingShare", "Spark of Agility": "TD_BlessingSpeed", "Cast to Last": "TD_BuffAndDebuffDuration", "One-Two Punch": "TD_ComboATarget", "Monumentalist": "TD_CreationSize", "Timeless Creator": "TD_CreationSizeLifeTime", "Deadeye": "TD_DistancePower", "Knife's Edge": "TD_EdgePower", "Specialized Training": "TD_EmpoweredHitsBuff", "Catalyst": "TD_EnergyCatalyst", "Egoist": "TD_EnergyConversion", "Fire Up!": "TD_EnergyDischarge", "Orb Ponderer": "TD_EnhancedOrbsCooldown", "Orb Dancer": "TD_EnhancedOrbsSpeed", "Super Surge": "TD_FasterDashes", "Chronoboost": "TD_FasterDashes2", "Explosive Entrance": "TD_FasterDashes3", "Missile Propulsion": "TD_FasterProjectiles", "Aerials": "TD_FasterProjectiles2", "Siege Machine": "TD_FasterProjectiles3", "Tempo Swing": "TD_HitAnythingRestoreStagger", "Stinger": "TD_HitEnemyBurnThem", "Hotshot": "TD_HitRockCooldown", "Stacks On Stacks": "TD_HitsIncreaseSpeedAndPower", "Fight Or Flight": "TD_HitSpeed", "Perfect Form": "TD_HitsReduceCooldowns", "OLD Unstoppable": "TD_IncreasedPowerWithMaxStagger", "Stagger Swagger": "TD_IncreasedSpeedWithStagger", "Prize Fighter": "TD_KOKing", "Twin Drive": "TD_MovementAbilityCharges", "Heavy Impact": "TD_MultiHitsReduceCooldowns", "Orb Replicator": "TD_OrbShare", "Rapid Fire": "TD_PrimaryAbilityCooldownReduction", "Primetime": "TD_PrimaryEcho", "Unstoppable": "TD_ResistFirstHit", "Recovery Drone": "TD_Revive", "Among Titans": "TD_ShrinkSelfGrowAllies", "Built Different": "TD_SizeIncrease", "Big Fish": "TD_SizeIncrease2", "Might of the Colossus": "TD_SizePowerConversion", "Extra Special": "TD_SpecialCooldownAfterRounds", "Rampage": "TD_StackingSize", "Reverberation": "TD_StaggerCooldownRateConversion", "Bulk Up": "TD_StaggerPowerConversion", "Peak Performance": "TD_StaggerSpeedConversion", "Quick Strike": "TD_StrikeCooldownReduction", "Team Player": "TD_StrikeRockTowardsAllies", "Adrenaline Rush": "TD_TakeDownReduceCooldowns"}
+
+
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find the section that contains "Former Awakenings"
+        former_awakenings_header = soup.find('span', id='Former_Awakenings')
+
+        # The table containing the former awakenings follows this header
+        awakenings_table = former_awakenings_header.find_next('table')
+
+        # Extract relevant awakening names from the table
+        awakening_names = []
+
+        # Loop through each row in the table to extract the awakening names
+        for row in awakenings_table.find_all('tr')[1:]:  # Skip the header row
+            name_column = row.find('td')
+            if name_column:
+                name = name_column.get_text(strip=True)
+                awakening_names.append(name)
+
+        # Replace awakening names with internal identifiers if found in the dict
+        out_of_rotation_awakenings = []
+        for name in awakening_names:
+            internal_id = dict_external_to_internal.get(name, name)  # Use name if not found in dict
+            out_of_rotation_awakenings.append(internal_id)
+
+        return out_of_rotation_awakenings
+    else:
+        print(f"Failed to retrieve the page. Status code: {response.status_code}")
+        return []
 
 
 
@@ -66,6 +107,8 @@ class MainWindow(tk.Tk):
         super().__init__()
         self.title("AwakeningTracker v0.0.17") #v0.0.17 10/14/2024
 
+        dict_internal_to_external = {"TD_AvoidDamageHitHarder": "Glass Cannon", "TD_BarrierBuff": "Demolitionist", "TD_BaseStaggerAndRegen": "Reptile Remedy", "TD_BlessingCooldownRate": "Spark of Focus", "TD_BlessingMaxStagger": "Spark of Resilience", "TD_BlessingPower": "Spark of Strength", "TD_BlessingShare": "Spark of Leadership", "TD_BlessingSpeed": "Spark of Agility", "TD_BuffAndDebuffDuration": "Cast to Last", "TD_ComboATarget": "One-Two Punch", "TD_CreationSize": "Monumentalist", "TD_CreationSizeLifeTime": "Timeless Creator", "TD_DistancePower": "Deadeye", "TD_EdgePower": "Knife's Edge", "TD_EmpoweredHitsBuff": "Specialized Training", "TD_EnergyCatalyst": "Catalyst", "TD_EnergyConversion": "Egoist", "TD_EnergyDischarge": "Fire Up!", "TD_EnhancedOrbsCooldown": "Orb Ponderer" , "TD_EnhancedOrbsSpeed": "Orb Dancer", "TD_FasterDashes": "Super Surge", "TD_FasterDashes2": "Chronoboost", "TD_FasterDashes3": "Explosive Entrance", "TD_FasterProjectiles": "Missile Propulsion", "TD_FasterProjectiles2": "Aerials", "TD_FasterProjectiles3":"Siege Machine", "TD_HitAnythingRestoreStagger": "Tempo Swing", "TD_HitEnemyBurnThem": "Stinger", "TD_HitRockCooldown": "Hotshot", "TD_HitsIncreaseSpeedAndPower": "Stacks On Stacks", "TD_HitSpeed": "Fight Or Flight", "TD_HitsReduceCooldowns": "Perfect Form", "TD_IncreasedPowerWithMaxStagger": "OLD Unstoppable", "TD_IncreasedSpeedWithStagger": "Stagger Swagger", "TD_KOKing": "Prize Fighter", "TD_MovementAbilityCharges": "Twin Drive", "TD_MultiHitsReduceCooldowns": "Heavy Impact", "TD_OrbShare": "Orb Replicator", "TD_PrimaryAbilityCooldownReduction": "Rapid Fire", "TD_PrimaryEcho": "Primetime", "TD_ResistFirstHit": "Unstoppable", "TD_Revive":"Recovery Drone", "TD_ShrinkSelfGrowAllies": "Among Titans", "TD_SizeIncrease": "Built Different", "TD_SizeIncrease2": "Big Fish", "TD_SizePowerConversion": "Might of the Colossus", "TD_SpecialCooldownAfterRounds": "Extra Special", "TD_StackingSize": "Rampage", "TD_StaggerCooldownRateConversion": "Reverberation", "TD_StaggerPowerConversion": "Bulk Up", "TD_StaggerSpeedConversion": "Peak Performance", "TD_StrikeCooldownReduction": "Quick Strike", "TD_StrikeRockTowardsAllies": "Team Player", "TD_TakeDownReduceCooldowns": "Adrenaline Rush"}
+
 
 
         self.master_copy_deck = ['TD_AvoidDamageHitHarder', 'TD_BarrierBuff', 'TD_BaseStaggerAndRegen', 'TD_BlessingCooldownRate', 'TD_BlessingMaxStagger', 'TD_BlessingPower', 'TD_BlessingShare', 'TD_BlessingSpeed', 'TD_BuffAndDebuffDuration', 'TD_ComboATarget', 'TD_CreationSize', 'TD_CreationSizeLifeTime', 'TD_DistancePower', 'TD_EdgePower', 'TD_EmpoweredHitsBuff', 'TD_EnergyCatalyst', 'TD_EnergyConversion', 'TD_EnergyDischarge', 'TD_EnhancedOrbsCooldown', 'TD_EnhancedOrbsSpeed', 'TD_FasterDashes', 'TD_FasterDashes2', 'TD_FasterDashes3', 'TD_FasterProjectiles', 'TD_FasterProjectiles2', 'TD_FasterProjectiles3', 'TD_HitAnythingRestoreStagger', 'TD_HitEnemyBurnThem', 'TD_HitRockCooldown', 'TD_HitsIncreaseSpeedAndPower', 'TD_HitSpeed', 'TD_HitsReduceCooldowns', 'TD_IncreasedSpeedWithStagger', 'TD_KOKing', 'TD_MovementAbilityCharges', 'TD_MultiHitsReduceCooldowns', 'TD_OrbShare', 'TD_PrimaryAbilityCooldownReduction', 'TD_PrimaryEcho', 'TD_ResistFirstHit', 'TD_Revive', 'TD_ShrinkSelfGrowAllies', 'TD_SizeIncrease', 'TD_SizeIncrease2', 'TD_SizePowerConversion', 'TD_SpecialCooldownAfterRounds', 'TD_StackingSize', 'TD_StaggerCooldownRateConversion', 'TD_StaggerPowerConversion', 'TD_StaggerSpeedConversion', 'TD_StrikeCooldownReduction', 'TD_StrikeRockTowardsAllies', 'TD_TakeDownReduceCooldowns']
@@ -74,14 +117,8 @@ class MainWindow(tk.Tk):
         # List of items to remove
         #
 
-        items_to_remove = [
-        'TD_ShrinkSelfGrowAllies', 'TD_StaggerPowerConversion', 'TD_BarrierBuff',
-        'TD_EnergyDischarge','TD_AvoidDamageHitHarder', 'TD_MultiHitsReduceCooldowns',
-        'TD_SizePowerConversion', 'TD_EnhancedOrbsSpeed', 'TD_EnhancedOrbsCooldown', 'TD_OrbShare',
-        'TD_StaggerSpeedConversion', 'TD_StrikeCooldownReduction', 'TD_Revive',
-        'TD_BaseStaggerAndRegen', 'TD_StaggerCooldownRateConversion', 'TD_BlessingShare',
-        'TD_EmpoweredHitsBuff', 'TD_HitEnemyBurnThem', 'TD_HitAnythingRestoreStagger'
-         ]
+        items_to_remove = extract_and_replace_awakenings(url = 'https://omegastrikers.wiki.gg/wiki/Awakenings')
+
 
     # Remove items from self.master_copy_deck
         self.master_copy_deck = [item for item in self.master_copy_deck if item not in items_to_remove]
